fcfcde56c56e6b5c642485efbb2eff240bf616be
BackcountryPrincess <DefaultUser418@hotmail.com>
2026-02-24 20:15:19 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Maple Sap Predictor</h1>
-  <p>Backend connection working.</p>
-`
+// Only keep this line if you actually have src/style.css
+import "./style.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
